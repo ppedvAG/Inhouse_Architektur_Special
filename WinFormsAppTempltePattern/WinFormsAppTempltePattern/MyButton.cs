40524d2a5917abb794fb527cd02208efb1d0402f
André <ruland@outlook.de>
2023-10-17 14:28:10 +0200

    MyWinFormsButton

---
+++ WinFormsAppTempltePattern/WinFormsAppTempltePattern/MyButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsAppTempltePattern
+{
+    internal class MyButton : Button
+    {
+
+        int paintCount;
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            pevent.Graphics.FillRectangle(Brushes.Aqua, ClientRectangle);
+            pevent.Graphics.FillEllipse(Brushes.Orange, ClientRectangle);
+
+            pevent.Graphics.DrawString($"Paint: {paintCount++}", SystemFonts.DefaultFont, Brushes.BlanchedAlmond, ClientRectangle);
+
+
+        }
+    }
+}
