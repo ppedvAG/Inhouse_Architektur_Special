73ac64c2c02add6d2fd29a22650bd6edfbf34a9e
André <ruland@outlook.de>
2023-10-17 11:26:53 +0200

    Factory

---
+++ HalloFactory/HalloFactory/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+
+Console.WriteLine("Hello, Factory!");
+
+string conStringSQL = "Server=(localdb)\\mssqllocaldb;Database=Northwnd;Trusted_Connection=true";
+
+string conStringlite = @"Data Source=C:\DB\Northwind.sqlite";
+
+DbProviderFactory factory = null;
+string conString = "";
+
+if (1 != 1)
+{
+    factory = Microsoft.Data.SqlClient.SqlClientFactory.Instance;
+    conString = conStringSQL;
+}
+else
+{
+    factory = Microsoft.Data.Sqlite.SqliteFactory.Instance;
+    conString = conStringlite;
+}
+
+
+DbConnection con = factory.CreateConnection();
+con.ConnectionString = conString;
+con.Open();
+Console.WriteLine("DB open");
+
+DbCommand sqlCommand = factory.CreateCommand();
+sqlCommand.Connection = con;
+sqlCommand.CommandText = "SELECT COUNT(*) FROM Employees";
+
+var rowCount = sqlCommand.ExecuteScalar();
+Console.WriteLine($"{rowCount} Employees in DB");
