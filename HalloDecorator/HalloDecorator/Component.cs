bf754bda077368f66471e1cdb028e96383c2f33f
André <ruland@outlook.de>
2023-10-17 11:48:29 +0200

    Deco

---
+++ HalloDecorator/HalloDecorator/Component.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloDecorator
+{
+    public interface IComponent
+    {
+        string Name { get; }
+        decimal Price { get; }
+    }
+
+    public class Pizza : IComponent
+    {
+        public string Name => "Pizza";
+
+        public decimal Price => 7.4m;
+    }
+
+    public class Brot : IComponent
+    {
+        public string Name => "Brot";
+
+        public decimal Price => 3.2m;
+    }
+
+    public abstract class Deco : IComponent
+    {
+        protected readonly IComponent parent;
+
+        public Deco(IComponent parent)
+        {
+            this.parent = parent;
+        }
+
+        public abstract string Name { get; }
+
+        public abstract decimal Price { get; }
+    }
+
+    public class Salami : Deco
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Salami";
+        public override decimal Price => parent.Price + 1.5m;
+    }
+
+
+    public class Käse : Deco
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Käse";
+        public override decimal Price => parent.Price + 1.2m;
+    }
+
+
+}
---
+++ HalloDecorator/HalloDecorator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDecorator;
+
+Console.WriteLine("Hello, World!");
+
+
+var p1 = new Käse(new Käse(new Salami(new Brot())));
+
+
+Console.WriteLine(p1.Name + "  " + p1.Price);
