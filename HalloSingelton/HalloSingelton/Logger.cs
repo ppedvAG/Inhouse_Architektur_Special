97bef145d65c3e0f8c724fdf4e021bd3ca232f5b
André <ruland@outlook.de>
2023-10-17 10:28:28 +0200

    Das Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    internal class Logger
+    {
+        private static Logger _instance = null;
+        private static Object _obj = new Object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    lock (_obj)
+                    {
+                        if (_instance == null)
+                            _instance = new Logger();
+                    }
+
+                return _instance;
+            }
+        }
+
+        private Logger()
+        {
+            Info("NEUER LOGGER");
+        }
+
+        public void Info(string msg)
+        {
+            Console.WriteLine($"[INFO] {DateTime.Now:g} {msg} ");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloSingelton;
+using System;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        Parallel.For(0, 20, i => Logger.Instance.Info($" {i} Hallo welt"));
+
+        Logger.Instance.Info("Hallo welt");
+        Logger.Instance.Info("Hallo welt");
+        Logger.Instance.Info("Hallo welt");
+        Logger.Instance.Info("Hallo welt");
+    }
+}
