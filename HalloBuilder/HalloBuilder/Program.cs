5b12ae437a64f70a73cb9246c6c62a1404227f99
André <ruland@outlook.de>
2023-10-17 11:17:07 +0200

    Builder

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+
+var s1 = new Schrank.Builder()
+                    .SetBöden(4)
+                    .SetTüren(5)
+                    .SetFarbe("gelb")
+                    .Create();
+
+var s2 = new Schrank.Builder()
+                    .SetBöden(2)
+                    .SetTüren(4)
+                    .SetOberfläche(Oberfläche.Gewachst)
+                    .Create();
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,67 @@
+﻿namespace HalloBuilder
+{
+    internal class Schrank
+    {
+        private Schrank()
+        { }
+
+        public int AnzahlTüren { get; private set; }
+        public Oberfläche Oberfläche { get; private set; }
+        public int AnzahlBöden { get; private set; }
+        public string Farbe { get; private set; }
+
+        internal class Builder
+        {
+            Schrank schrank = new Schrank();
+            public Schrank Create() { return schrank; } 
+
+            public Builder SetTüren(int anzTüren)
+            {
+                if (anzTüren < 0 || anzTüren > 8)
+                    throw new ArgumentException("Ungültige Anzahl Türen");
+
+                schrank.AnzahlTüren = anzTüren;
+
+                return this;
+            }
+
+            public Builder SetBöden(int anzBöden)
+            {
+                if (anzBöden < 2 || anzBöden > 6)
+                    throw new ArgumentException("Bla böden!!!");
+
+                schrank.AnzahlBöden = anzBöden;
+
+                return this;
+            }
+
+            public Builder SetFarbe(string farbe)
+            {
+                schrank.Oberfläche = Oberfläche.Lackiert;
+                schrank.Farbe = farbe;
+
+                return this;
+            }
+
+            public Builder SetOberfläche(Oberfläche oberfläche)
+            {
+                schrank.Oberfläche = oberfläche;
+
+                if (oberfläche == Oberfläche.Gewachst || oberfläche == Oberfläche.Natur)
+                    schrank.Farbe = "";
+
+                return this;
+            }
+
+
+        }
+
+
+    }
+    enum Oberfläche
+    {
+        Natur,
+        Gewachst,
+        Lackiert
+    }
+}
