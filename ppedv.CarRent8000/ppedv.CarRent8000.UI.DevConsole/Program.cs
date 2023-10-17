627dc4e5c1d12a96a0c3aed8685512b16b276791
André <ruland@outlook.de>
2023-10-17 16:30:26 +0200

    Bestes UI

---
+++ ppedv.CarRent8000/ppedv.CarRent8000.UI.DevConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ppedv.CarRent8000.Logic;
+using ppedv.CarRent8000.Model.Contracts;
+using ppedv.CarRent8000.Model.DomainModel;
+string conString = "Server=(localdb)\\mssqllocaldb;Database=CarRent8000_dev;Trusted_Connection=true;";
+
+Console.WriteLine("Hello, World!");
+
+IRepository repo = new ppedv.CarRent8000.Data.EfCore.EfContextRepositoryAdapter(conString);
+var rentService = new RentService(repo);
+
+foreach (var car in repo.GetAll<Car>())
+{
+    if (rentService.IsCarAvailable(car, DateTime.Now, DateTime.Now.AddDays(1)))
+        Console.Write("JA ");
+    else
+        Console.Write("NEIN ");
+
+    Console.WriteLine($"{car.Manufacturer} {car.Model} {car.Color}");
+}
